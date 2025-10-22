521fb07b77674cd346ec645855b61853511b5898
Zohaib Khan <zohaibk1111@gmail.com>
2025-10-22 01:42:49 -0400

    locally reauthenticated gmail

--- langgraph/main.py
+++ langgraph/main.py
@@ -6,8 +6,7 @@ def main():
     load_dotenv()
 
     graph = build_graph()
-    graph.invoke({
-    }, {"recursion_limit": 200})
+    graph.invoke({"status": "init"}, {"recursion_limit": 200})
 
 if __name__ == "__main__":
     main()
